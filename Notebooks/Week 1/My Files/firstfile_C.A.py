aba736854706c5a3b6f1822b8c115be2d87a1e5a
Clahrens <charlotte.ahrens@epfl.ch>
2022-09-25 19:26:43 +0200

    Some changes

---
+++ Notebooks/Week 1/My Files/firstfile_C.A.py
@@ -0,0 +1,9 @@
+#Usual Hello World 
+print ("Hello, world")
+
+#The first few lines from teh ZEn of Python by Tim Peters
+print('Beautiful is better than ugly.')
+print('Explicit is better than implicit.')
+print('Simple is better than complex.')
+print('Complex is better than complicated.')
+
